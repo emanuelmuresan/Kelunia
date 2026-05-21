f14189092ee5997c88ca9fcca47ba4a11f58b907
Emanuel Muresan <emanuelmuresan@gmail.com>
2026-05-21 05:17:13 +0300

    Align Android login experience

--- android/app/src/main/java/com/kelunia/app/MainActivity.java
+++ android/app/src/main/java/com/kelunia/app/MainActivity.java
@@ -1,5 +1,17 @@
 package com.kelunia.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getBridge() != null && getBridge().getWebView() != null) {
+            getBridge().getWebView().postDelayed(() -> {
+                getBridge().getWebView().loadUrl(getBridge().getLocalUrl() + "/login.html");
+            }, 250);
+        }
+    }
+}
